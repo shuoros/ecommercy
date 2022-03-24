29712a06e1a8993fe283ee6581dad92730942467
Soroush <s_shemshadi@ymail.com>
2022-03-24 13:12:06 +0430

    IMPLEMENT(User) Create a DAO represent User entity.

---
+++ src/main/java/io/github/shuoros/ecommercy/dao/User.java
@@ -0,0 +1,49 @@
+package io.github.shuoros.ecommercy.dao;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "users", schema = "ecommercy")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @Builder.Default
+    @ElementCollection
+    private List<String> roles = List.of("USER");
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+}
