3f838980468be836074ca9a02e5324631e1b2fd5
Soroush <s_shemshadi@ymail.com>
2022-03-24 11:17:31 +0430

    INIT(project) add a pom.xml based maven project management system and dependencies.

---
+++ src/main/java/io/github/shuoros/ecommercy/EcommercyApplication.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.ecommercy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercyApplication.class, args);
+    }
+
+}
