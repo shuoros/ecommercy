9d28ae9d7f478de7b6bce5d49e39d96bdc83285f
Soroush <s_shemshadi@ymail.com>
2022-03-28 18:45:28 +0430

    UPDATE(UserDetails) Add support of admin authentication.

--- src/main/java/io/github/shuoros/ecommercy/security/jwt/UserDetails.java
+++ src/main/java/io/github/shuoros/ecommercy/security/jwt/UserDetails.java
@@ -1,6 +1,8 @@
 package io.github.shuoros.ecommercy.security.jwt;
 
+import io.github.shuoros.ecommercy.dao.Admin;
 import io.github.shuoros.ecommercy.dao.User;
+import io.github.shuoros.ecommercy.dao.service.AdminService;
 import io.github.shuoros.ecommercy.dao.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -19,25 +21,33 @@ import java.util.stream.Collectors;
 public class UserDetails implements UserDetailsService {
 
     private final UserService userService;
+    private final AdminService adminService;
 
     @Autowired
-    public UserDetails(UserService userService) {
+    public UserDetails(UserService userService, AdminService adminService) {
         this.userService = userService;
+        this.adminService = adminService;
     }
 
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String name)
             throws UsernameNotFoundException {
-        User user;
-        if (userService.getUser(name).isPresent())
-            user = userService.getUser(name).get();
-        else
+        if (userService.get(name).isPresent()) {
+            User user = userService.get(name).get();
+            return getUserDetails(user.getRoles(), user.getEmail(), user.getPassword());
+        } else if (adminService.get(name).isPresent()) {
+            Admin admin = adminService.get(name).get();
+            return getUserDetails(admin.getRoles(), admin.getEmail(), admin.getPassword());
+        } else
             throw new UsernameNotFoundException("Invalid email or password.");
+    }
 
-        List<String> listRoles = new ArrayList<>(user.getRoles());
+    private org.springframework.security.core.userdetails.UserDetails getUserDetails(List<String> roles, String email, String password) {
+        List<String> listRoles = new ArrayList<>(roles);
         List<GrantedAuthority> authorities = listRoles.stream().map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+        return new org.springframework.security.core.userdetails.User(email, password,
                 authorities);
     }
+
 }
