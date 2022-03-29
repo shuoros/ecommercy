ac1e22dcb831419c33f891939d67ba99824de887
Soroush <s_shemshadi@ymail.com>
2022-03-29 17:22:57 +0430

    IMPLEMENT(Category) Create a DAO represent Category entity.

---
+++ src/main/java/io/github/shuoros/ecommercy/dao/Category.java
@@ -0,0 +1,28 @@
+package io.github.shuoros.ecommercy.dao;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categories", schema = "ecommercy")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+}
