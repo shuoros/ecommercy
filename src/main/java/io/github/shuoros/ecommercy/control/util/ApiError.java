dcc73cb78a13b220b66a801d461ea3fac1c26abf
Soroush <s_shemshadi@ymail.com>
2022-04-28 12:31:49 +0430

    IMPLEMENT(ApiError) Implement a class to simulate the spring's default error message.

---
+++ src/main/java/io/github/shuoros/ecommercy/control/util/ApiError.java
@@ -0,0 +1,61 @@
+package io.github.shuoros.ecommercy.control.util;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@JsonPropertyOrder({ "timestamp", "status", "error", "message", "path" })
+public class ApiError {
+
+    @JsonSerialize(using = ZonedDateTimeSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    private ZonedDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    private ApiError() {
+        timestamp = ZonedDateTime.now();
+    }
+
+    public ApiError(HttpStatus status, String path) {
+        this();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = "Unexpected error";
+        this.path = path;
+    }
+
+    public ApiError(HttpStatus status, String path, String message) {
+        this();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+        this.path = path;
+    }
+
+    public String convertToJson() throws JsonProcessingException {
+        if (this == null) {
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper.writeValueAsString(this);
+    }
+
+}
