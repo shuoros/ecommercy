942976060af689fa26c33d99f051b1eddd6555b8
Soroush <s_shemshadi@ymail.com>
2022-05-09 20:46:57 +0430

    UPDATE(JwtAuthenticationFilter) Remove redundant slf4j annotation.

--- src/main/java/io/github/shuoros/ecommercy/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/io/github/shuoros/ecommercy/security/filter/JwtAuthenticationFilter.java
@@ -19,7 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Slf4j
 @Service
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
