dd38e565eecb9ec7518d2565c897ac913e9e4e6f
Soroush <s_shemshadi@ymail.com>
2022-05-25 10:07:23 +0430

    UPDATE(CategoryRepository) Enable RepositoryRestResource for Spring-Data-Rest.

--- src/main/java/io/github/shuoros/ecommercy/dao/repository/CategoryRepository.java
+++ src/main/java/io/github/shuoros/ecommercy/dao/repository/CategoryRepository.java
@@ -2,13 +2,10 @@ package io.github.shuoros.ecommercy.dao.repository;
 
 import io.github.shuoros.ecommercy.dao.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    Optional<Category> findByName(String name);
-
+@RepositoryRestResource(collectionResourceRel = "category", path = "category")
+public interface CategoryRepository extends JpaRepository<Category, String> {
 }
