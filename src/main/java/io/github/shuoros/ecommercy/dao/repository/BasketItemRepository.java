c3e98e405cf385f05d426de7d3593241f422ef18
Soroush <s_shemshadi@ymail.com>
2022-05-25 10:14:34 +0430

    IMPLEMENT(BasketItemRepository) Implement a JpaRepository for persisting basket item entity and enable RepositoryRestResource for Spring-Data-Rest.

---
+++ src/main/java/io/github/shuoros/ecommercy/dao/repository/BasketItemRepository.java
@@ -0,0 +1,11 @@
+package io.github.shuoros.ecommercy.dao.repository;
+
+import io.github.shuoros.ecommercy.dao.BasketItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RepositoryRestResource(collectionResourceRel = "basketItem", path = "basketItem")
+public interface BasketItemRepository extends JpaRepository<BasketItem, String> {
+}
