b9f4a312a21937d1534e96cd4d1a0231020db9e7
Soroush <s_shemshadi@ymail.com>
2022-05-25 10:09:14 +0430

    UPDATE(Product) Update some fields and add group and categories relations.

--- src/main/java/io/github/shuoros/ecommercy/dao/Product.java
+++ src/main/java/io/github/shuoros/ecommercy/dao/Product.java
@@ -1,14 +1,15 @@
 package io.github.shuoros.ecommercy.dao;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.github.shuoros.ecommercy.dao.util.StringListConverter;
 import lombok.*;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -28,40 +29,51 @@ public class Product {
     @Column(updatable = false, nullable = false, columnDefinition = "VARCHAR(255)")
     private String id;
 
-    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "group_id", referencedColumnName = "id", nullable = false)
+    private Group group;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id", referencedColumnName = "id", nullable = false)
+    private List<Category> categories;
+
+    @Column(nullable = false)
     private String name;
 
     private String description;
 
-    @NotNull
-    private double price;
+    @Column(nullable = false)
+    private Float price;
 
     @Builder.Default
-    @NotNull
-    private double discount = 0.0;
+    @Column(nullable = false)
+    private Float discount = 0.0F;
 
     @Builder.Default
-    @NotNull
+    @Column(nullable = false)
     private int point = 0;
 
-    @NotNull
-    private int inventory;
-
-    @NotNull
-    private String thumbnail;
-
-    @NotNull
-    @Convert(converter = StringListConverter.class)
-    private List<String> images;
+    @Column(nullable = false)
+    private Integer inventory;
 
     @OneToMany(mappedBy = "product")
     @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private List<Comment> comments;
 
-    private int stars;
+    @Builder.Default
+    @Column(nullable = false)
+    private float stars = -1;
 
-//    @OneToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "category_id", referencedColumnName = "id")
-//    private Category category;
+    @Builder.Default
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt = new Date();
+
+    @Builder.Default
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt = new Date();
 
 }
