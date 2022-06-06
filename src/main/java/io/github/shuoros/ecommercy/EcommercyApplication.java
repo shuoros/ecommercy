31ce2645722f0a1c56b3bcecc9c46ec7f8268a96
Soroush <s_shemshadi@ymail.com>
2022-06-06 14:20:29 +0430

    UPDATE(EcommercyApplication) Add application-mysql.properties as a property source.

--- src/main/java/io/github/shuoros/ecommercy/EcommercyApplication.java
+++ src/main/java/io/github/shuoros/ecommercy/EcommercyApplication.java
@@ -2,8 +2,14 @@ package io.github.shuoros.ecommercy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:application-mysql.properties")
+})
 public class EcommercyApplication {
 
     public static void main(String[] args) {
