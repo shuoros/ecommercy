e545a31bea3de5f7f3097010fc48065e3d879d40
Soroush <s_shemshadi@ymail.com>
2022-06-14 10:53:41 +0430

    UPDATE(AbstractContainerBaseTest) Update MySQL test container configurations.

--- src/test/java/io/github/shuoros/ecommercy/AbstractContainerBaseTest.java
+++ src/test/java/io/github/shuoros/ecommercy/AbstractContainerBaseTest.java
@@ -1,15 +1,28 @@
 package io.github.shuoros.ecommercy;
 
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@DirtiesContext
 public class AbstractContainerBaseTest {
 
-    static final MySQLContainer MY_SQL_CONTAINER;
+    @Container
+    public static MySQLContainer<?> mySqlDB = new MySQLContainer<>
+            ("mysql:5.7.37")
+            .withDatabaseName("ecommercy")
+            .withUsername("admin")
+            .withPassword("admin");
 
-    static {
-        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
-
-        MY_SQL_CONTAINER.start();
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySqlDB::getJdbcUrl);
+        registry.add("spring.datasource.username", mySqlDB::getUsername);
+        registry.add("spring.datasource.password", mySqlDB::getPassword);
     }
 
 }
